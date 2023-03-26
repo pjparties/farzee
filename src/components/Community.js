e780fa822738566fec023ebfc0ada8e59fd94572
parth <admin@test.com>
2023-03-27 01:38:28 +0530

    just need to center the bar now hehe

--- src/components/Community.js
+++ src/components/Community.js
@@ -1,34 +1,42 @@
 import React, { useState, useEffect } from "react";
 import "../styles/Community.css";
-import {AiFillLike} from "react-icons"
+import { AiFillLike } from "react-icons/ai";
+import { AiFillDislike } from "react-icons/ai";
 
 const LikeDislike = () => {
-  const [likes, setLikes] = useState(0);
-  const [dislikes, setDislikes] = useState(0);
-  const [ratioLikes, setRatioLikes] = useState(0);
+    const [likes, setLikes] = useState(0);
+    const [dislikes, setDislikes] = useState(0);
+    const [ratioLikes, setRatioLikes] = useState(0);
 
-  useEffect(() => {
+    useEffect(() => {
     const total = likes + dislikes;
     const ratio = likes / total;
     setRatioLikes(ratio);
-  }, [likes, dislikes]);
+    }, [likes, dislikes]);
 
-  return (
-    <div>
-      <div className="bar-container">
-        <div
-          className="like-bar"
-          style={{
+    return (
+    <div className="bar-main">
+
+        <div className="bar-container">
+            <div className="like-bar" style={{
             width: `${ratioLikes * 100}%`,
-          }}
-        ></div>
-      </div>
-      <button id="like-button" onClick={() => setLikes(likes + 1)}>Like</button>
-      <button id="dislike-button" onClick={() => setDislikes(dislikes + 1)}>Dislike</button>
-      <p>{likes} likes</p>
-      <p>{dislikes} dislikes</p>
+            }}></div>
+        
+        </div>
+            <div className="like-dislike-container">
+                
+                <div className="like-container">
+                    <p>{likes}</p>
+                    <AiFillLike className="like-button" onClick={() => setLikes(likes + 1)}/>
+                </div>
+            
+                <div className="dislike-container">
+                    <p>{dislikes} </p>
+                    <AiFillDislike classname="dislike-button" onClick={() => setDislikes(dislikes + 1)}/>
+                </div>
+            </div>
     </div>
-  );
+    );
 };
 
 export default LikeDislike;
