e26da2a11aa7d1a20671bf21e21dc55d2958792b
Lakshay Arora <lakshay16199@gmail.com>
2023-03-26 19:36:34 +0530

    home

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,13 +1,28 @@
-import React from 'react'
+import React,{useState} from 'react'
 import Navbar from './Navbar'
+import big from '../assets/logo-big.png'
 
 import '../styles/Home.css'
 
 function Home() {
+  const[inp,setInp]=useState('')
+  const reset=(e)=>{
+    e.preventDefault();
+    setInp('');
+  }
   return (
-    <div className = 'bg'>
-        <Navbar/>
-        Home
+    <div className='bg'>
+      <Navbar />
+      <img className='logo' src={big} alt='logo' />
+      <div className='text_box'>
+        <form action="#">
+          <input value={inp} type="text" onChange={e=>setInp(e.target.value)} placeholder="Check if your SMS or phone number is in data breach" className='box' />
+          <div className='submission'>
+            <button onClick={reset} >Reset</button>
+            <button type='submit'>Check</button>
+        </div>
+        </form>
+      </div>
     </div>
   )
 }
