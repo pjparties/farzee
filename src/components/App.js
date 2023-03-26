507be277b73e41d74c47832196899939a4302f93
parth <admin@test.com>
2023-03-26 17:16:58 +0530

    size error bg

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,12 @@
 import React from 'react'
 import '../styles/App.css';
+import Home from './Home';
 
 function App() {
   return (
-    <div>Hello World</div>
+    <div>
+      <Home/>
+    </div>
   )
 }
 
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import Navbar from './Navbar'
 
+import '../styles/Home.css'
 
 function Home() {
   return (
-    <div>
+    <div className = 'bg'>
         <Navbar/>
         Home
     </div>
