5e1e85ff929d865f18e0f64be470262f713ea419
Lakshay Arora <lakshay16199@gmail.com>
2023-03-28 12:42:01 +0530

    details pg

--- src/components/Details.js
+++ src/components/Details.js
@@ -1,11 +1,23 @@
 import React from 'react'
 import Navbar from './Navbar'
+import flowchart from '../assets/flowchart.png'
+import protocol from '../assets/protocol.png'
+import roadmap from '../assets/roadmap.png'
+import '../styles/Details.css'
 
 function Details() {
     return (
         <div className='container'>
             <Navbar />
-            <p>Details</p>
+            <div className="title">
+                <h1>CITATION</h1>
+            </div>
+            <div className="body">
+                <img className='img1' src={protocol} alt="protocol flowchart" />
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe incidunt eius rem quae quas minima, perferendis assumenda, suscipit ad impedit nulla animi delectus quidem dolores ipsum veniam voluptate mollitia corporis?</p>
+            </div>
+
+            <div className="flowchart"></div>
         </div>
     )
 }
